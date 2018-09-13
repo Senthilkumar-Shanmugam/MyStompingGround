80005c5960c37c381ed22abe6538f9fd4db2f94a
tribles <senthilkumar.s.shanmugam@oracle.com>
2018-09-13 17:10:15 +0530

    queue implementation using array

--- DataStructures/src/main/java/com/learn/datastructures/linear/QueueWithArray.java
+++ DataStructures/src/main/java/com/learn/datastructures/linear/QueueWithArray.java
@@ -8,10 +8,68 @@ package com.learn.datastructures.linear;
  *
  */
 public class QueueWithArray {
+	 private int capacity; 
+	 private int[] items;
+	 int size=0;
+	 int rear=-1;// we insert at the rear , so initailly its is -1
+	 int front=0;
+	 
+	 public QueueWithArray(int capacity){
+		 this.capacity=capacity;
+		 items= new int[capacity];
+	 }
+	 
+	 public boolean isQueueEmpty(){
+		 return  size==0;
+	 }
+	 
+	 public boolean isQueueFull(){
+		 return size==capacity;
+	 }
+	 
+	 public void enqueue(int item){
+		 if(isQueueFull()){
+			 System.out.println("Queue is full");
+			 return;
+		 }
+		 rear++;
+		 if(rear==size-1)
+			 rear=0;
+		 items[rear]=item;
+		 size++;
+		 System.out.println("Element " + item+ " is pushed to Queue !");
+		 
+	 }
+	 public void dequeue(){
+		 if(isQueueEmpty()){
+			 System.out.println("Queue is empty");
+			 return ;
+		 }
+		 front++;
+		 if(front==size-1){
+			 System.out.println("Pop operation done ! removed: "+items[front-1]);
+			 front = 0;
+		 }else{
+			 System.out.println("Pop operation done ! removed: "+items[front-1]);
+		 }
+		 size--;
+	 }
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		QueueWithArray queue = new QueueWithArray(4);
+		queue.enqueue(4);
+		queue.dequeue();
+        queue.enqueue(56);
+        queue.enqueue(2);
+        queue.enqueue(67);
+        queue.dequeue();
+        queue.dequeue();
+        queue.enqueue(24);
+        queue.dequeue();
+        queue.enqueue(98);
+        queue.enqueue(45);
+        queue.enqueue(23);
+        queue.enqueue(435);
 	}
 
 }
